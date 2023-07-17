31c72aab6a4671873e110e7d1085bd0b92250b1e
kiinosuke13 <kii613.penguin@gmail.com>
2023-07-18 08:31:14 +0900

    ファイルを変更

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-'use strict';
-
+"use strict";
 function memo() {
-    let memo = prompt('入力して下さい');
-    let li = document.createElement('li');
-    li.textContent =  memo;
-    const memoList = document.getElementById('memo_list');
-    memoList.appendChild(li);
+  let memo = prompt("入力してください");
+  let li = document.createElement("li");
+  li.textContent = memo;
+  const memoList = document.getElementById("memo_list");
+  memoList.appendChild(li);
 }
 
-const btn = document.querySelector('button');
-btn.addEventListener('click',memo);
+const btn = document.querySelector("button");
+btn.addEventListener("click", memo);
